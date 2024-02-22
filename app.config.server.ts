7bbec1042c486b4d55f07d65b3c2e8b6563ae414
JanJezewski01 <jezewski533@gmail.com>
2024-02-22 14:32:00 +0100

    first commit

---
+++ app.config.server.ts
@@ -0,0 +1,13 @@
+import { mergeApplicationConfig, ApplicationConfig, Injectable } from '@angular/core';
+import { provideServerRendering } from '@angular/platform-server';
+import { appConfig } from './app.config';
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering()
+  ]
+};
+
+
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
---
+++ coffe/coffe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffe',
+  standalone: true,
+  imports: [],
+  templateUrl: './coffe.component.html',
+  styleUrl: './coffe.component.css'
+})
+export class CoffeComponent {
+
+}
